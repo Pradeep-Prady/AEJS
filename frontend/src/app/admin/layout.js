f6bb2c985d97941a30eb559fe7671bce0fc78b34
Pradeep-Prady <pradeepprady005@gmail.com>
2024-08-05 12:32:35 +0530

    admin page message added

--- frontend/src/app/admin/layout.js
+++ frontend/src/app/admin/layout.js
@@ -5,20 +5,27 @@ import SideBar from "@/components/Admin/SideBar";
 import React, { useState } from "react";
 
 const layout = ({ children }) => {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="w-full h-screen  flex items-center  ">
       <div className="w-full h-full  hidden md:flex items-center  ">
         <div className="w-[22%]">
-          <SideBar setIsLoggedIn={setIsLoggedIn}/>
+          <SideBar setIsLoggedIn={setIsLoggedIn} />
         </div>
-        <AdminMain isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} children={children} />
+        <AdminMain
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          children={children}
+        />
       </div>
 
       <div className="w-full h-full  flex md:hidden items-center  justify-center">
-        <p>Only Vi </p>
+        <p>
+          {" "}
+          This admin interface is optimized for desktop use. For the best
+          experience, please access it on a desktop or laptop device.
+        </p>
       </div>
     </div>
   );
